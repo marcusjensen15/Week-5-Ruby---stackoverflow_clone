33dfc4457f4943dc2399045b976a575398738e1e
Student <example@example.com>
2020-01-21 11:28:14 -0800

    add factorybot

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory(:user) do
+    user_name {'Michael Bluth'}
+    email {'test5@test5.com'}
+    password {'test'}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe User do
   it { should have_many(:questions) }
+
+  it 'confirms the user_name' do
+    user = FactoryBot.create(:user)
+    expect(user.user_name).to eq 'Michael Bluth'
+  end
+  
 end
