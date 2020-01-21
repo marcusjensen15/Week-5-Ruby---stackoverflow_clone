4859949240c13312ec7ec51561acc9e1f760b995
Student <example@example.com>
2020-01-21 10:31:16 -0800

    config routes, migrate db, add helper file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  # root to: ''
+  # root to:
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
   resources :users do
     resources :questions
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_171603) do
+ActiveRecord::Schema.define(version: 2020_01_21_180513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 2020_01_21_171603) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
+    t.boolean "admin"
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
   end
 
 end
